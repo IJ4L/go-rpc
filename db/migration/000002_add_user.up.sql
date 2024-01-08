ccf9ed13f8d644aef3487f356537866b3b0acbb4
ijull <rijal9246@gmail.com>
2024-01-08 10:31:02 +0800

    feat: build and push to ECR

--- db/migration/000002_add_user.up.sql
+++ db/migration/000002_add_user.up.sql
@@ -10,5 +10,4 @@ CREATE TABLE
 
 ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
--- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
 ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
